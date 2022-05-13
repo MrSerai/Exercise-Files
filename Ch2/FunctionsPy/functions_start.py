149c5f001993e44d2ead765704a30d9275d179c0
MrSerai <allucious7@gmail.com>
2022-05-13 10:30:34 +0200

    done with functions and namedparams

--- Ch2/FunctionsPy/functions_start.py
+++ Ch2/FunctionsPy/functions_start.py
@@ -2,3 +2,29 @@
 
 # functions are defined using the def keyword, then
 # the : character and whitespace define the function body
+def a_func():
+    print("this is a line in my function that returns a value")
+    return 10258
+
+def my_returnable_func(a,b):
+    print("this is a line in my void function without a return statement")
+    print(a+b)
+
+
+#this function  contains an args paramater that is sort of like an array
+def variable_arguments(a,b,*args):
+    print(a+b)
+    for arg in args:
+        print(arg)
+
+
+
+
+#returned_val=a_func()
+#instead of assigning the returned value to a variable It just displays directly
+print(a_func())
+
+print(my_returnable_func(10,5))
+
+
+variable_arguments(29,21,50,"sdcasz",789456,"HELLO wORLD")
--- Ch2/FunctionsPy/namedparams_start.py
+++ Ch2/FunctionsPy/namedparams_start.py
@@ -7,3 +7,17 @@
 # Functions can then be called using named parameters
 # once you use a named parameter, all following params must be
 # called by name as well
+
+
+#you can also assign variable defaults in the parameters of the function, this will overide what was sent in the reference.
+def named_params_with_defaults(name,age,allow_access=False):
+    if allow_access:
+        print("age comes first")
+        print(age)
+        print(name)
+    else:
+        print("name comes first")
+        print(name)
+        print(age)
+#you can also assign variables  within the function reference
+named_params_with_defaults("Allucious",age=18,allow_access=True)
